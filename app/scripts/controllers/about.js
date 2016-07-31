a86552289ff7ab154eb87b9ab7975c8206b3295d
Sergiu Ciobanasu <szergen@gmail.com>
2016-07-31 19:43:17 +0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reMinderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reMinderApp
+ */
+angular.module('reMinderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reMinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the reMinderApp
+ */
+angular.module('reMinderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
