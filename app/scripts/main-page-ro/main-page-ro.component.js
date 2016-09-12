78451eac2419fe39ddec4a9595f7818d2ac7c7e7
Sergiu Ciobanasu <szergen@gmail.com>
2016-09-12 18:09:27 +0300

    Finished the basic styles for all the sections. cols. Slider. SideNav start.

--- app/scripts/main-page-ro/main-page-ro.component.js
+++ app/scripts/main-page-ro/main-page-ro.component.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.
     module('mainPageRo').
         component('mainPageRo', {
@@ -5,7 +6,27 @@ angular.
             controller: ['$http', function mainPageRoController($http) {
                 var self = this;
                 self.test = "traklalalalala";
+                var wheight = $(window).height()-100;
                 
+        $('.fullHeight').css('height', wheight);
+        console.log("slider ba");
+
+        $('#myCarousel .item img').each(function() {
+                var imgSrc = $(this).attr('src');
+            console.log(imgSrc);
+                $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+                $(this).remove();
+                console.log("a mers");
+            });
+              
+        $(window).resize(function(){
+                wheight = $(window).height();
+                $('.fullHeight').css('height', wheight);
+            });
+        $('.carousel').carousel({
+          interval: 5000
+        })
+              
                 /*   
                 $http.get('phones/phones.json').then(function(response) {
                     self.phones = response.data;
